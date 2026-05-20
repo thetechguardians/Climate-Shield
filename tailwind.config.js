3214a6750e6e572d9ed46c63ac8452a93ddac3ea
Ayush <ayushkr.sinha840@gmail.com>
2026-05-20 15:59:03 +0530

    Optimize frontend performance and improve accessibility

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+
+  content: [
+    "./Frontend/*.html",
+    "./Frontend/*.js"
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+}
