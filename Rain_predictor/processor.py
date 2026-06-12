14c80a71c96f35d2bd916d6d3b267f4f6a72d9a9
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-12 13:01:23 +0530

    Update processor.py

--- Rain_predictor/processor.py
+++ Rain_predictor/processor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import(train_test_split,RandomizedSearchCV,TimeSeriesSplit)
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
