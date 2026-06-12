7691f003fd1cb67b64d4761e583be761265c43dd
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-12 17:05:21 +0530

    Create model.py

---
+++ Rain_predictor/model.py
@@ -0,0 +1,59 @@
+import requests
+import pandas as pd
+from geopy.geocoders import Nominatim
+import joblib
+model=joblib.load("xgb_model_weather.joblib")
+def get_coordinates(location_dict):
+  city=location_dict.get("city","").strip()
+  state=location_dict.get("state","").strip()
+  country=location_dict.get("country","").strip()
+  geolocator = Nominatim(user_agent="weather_predictor_2026")
+  location_string=f"{city}, {state}, {country}".strip(",")
+
+  try:
+    location=geolocator.geocode(location_string)
+    if not location:
+      location_string=f"{state},{country}".strip(",")
+      location=geolocator.geocode(location_string)
+    if not location:
+      location_string=country
+      location=geolocator.geocode(location_string)
+    return location.latitude,location.longitude
+  except Exception as e:
+    return None,None
+
+def extract_features(location_dict):
+  lat,lon=get_coordinates(location_dict)
+  if not lat or not lon:
+    return "Coordinates not fetched."
+  API_KEY=""
+  url=f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
+  response=requests.get(url).json()
+  if response.get('cod') != 200:
+    return "Failed api call"
+  openweather_temp=response['main']['temp']
+  openweather_humidity = response['main']['humidity']
+  openweather_wind_speed = response['wind']['speed']*3.6
+  openweather_wind_dir = response['wind']['deg']
+  openweather_gusts = response['wind'].get('gust', openweather_wind_speed * 1.2)*3.6
+  lat_rad = np.radians(lat)
+  lon_rad = np.radians(lon)
+  coord_x= np.cos(lat_rad) * np.cos(lon_rad)
+  coord_y= np.cos(lat_rad) * np.sin(lon_rad)
+  coord_z= np.sin(lat_rad)
+
+  live_features = pd.DataFrame({
+"latitude":lat,
+"longitude":lon,
+"wind_direction_10m_dominant": openweather_wind_dir,
+"wind_speed_10m_mean": openweather_wind_speed,
+"wind_gusts_10m_mean": openweather_gusts,
+"temperature_2m_mean": openweather_temp,
+"relative_humidity_2m_mean": openweather_humidity,
+"coord_x":coord_x,
+"coord_y":coord_y,
+"coord_z":coord_z}, index=[0])
+  result=model.predict(live_features)
+  return result
+
+    
